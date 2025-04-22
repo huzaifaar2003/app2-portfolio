56ed1211bbd6ad786e15bdeeabdf3a510888b319
Huzaifa <huzaifaar2003@gmail.com>
2025-04-22 22:24:36 +0500

    Add Descriptions, Images, and Links to the Webpages

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import time
+import webbrowser
+
 
 col1, col2 = st.columns(2)
 
@@ -9,7 +11,10 @@ with col1:
 
 with col2:
     st.title("Huzaifa Abdul Rab")
-    content ="""Hi! Hope you're having a great day! Huzaifa Abdul Rab here. I'm a 2025 Mechatronics Engineering Graduate from NUST. Gradually shifting from Mechatronics to the Fields of Data In Sha Allah.  
+    content ="""Hi! Hope you're having a great day! Huzaifa Abdul Rab here. 
+    I'm a 2025 Mechatronics Engineering Graduate from NUST. 
+    Gradually shifting from Mechatronics to the Fields of Data In Sha Allah, I've started by learning Python.
+    Join me on learning journey!  
     """
     # """...""" for multiline (dynamically and automatically sizing) string.
     st.info(content)
@@ -24,9 +29,18 @@ df = pd.read_csv("data.csv", sep=";")
 col3, col4 = st.columns(2)
 
 with col3:
-    for index, row in df[0:10].iterrows():
-        st.subheader(row[0])
+    for index, row in df[:10].iterrows():
+        st.header(f"{index+1} - {row["title"]}")
+        st.subheader(row["description"])
+        st.write(row["url"])
+        st.image(f"images/{row["image"]}")
+        st.write("__________", font = "Helvetica")
+
 
 with col4:
-    for index, row in df[10:20].iterrows():
-        st.subheader(row[0])
+    for index, row in df[10:].iterrows():
+        st.header(f"{index+1} - {row["title"]}")
+        st.subheader(row["description"])
+        st.write(row["url"])
+        st.image(f"images/{row["image"]}")
+        st.write("__________", font="Helvetica")
