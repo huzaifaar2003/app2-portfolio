644df99c7575f74b0367561085a012a6b9235584
Huzaifa <huzaifaar2003@gmail.com>
2025-04-22 18:04:09 +0500

    Added columns and initial content to webpage

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/HAR_photo.jpg")
+
+with col2:
+    st.title("Huzaifa Abdul Rab")
+    content ="""Hi! Hope you're having a great day! Huzaifa Abdul Rab here. I'm a 2025 Mechatronics Engineering Graduate from NUST. Gradually shifting from Mechatronics to the Fields of Data In Sha Allah.  
+    """
+    # """...""" for multiline (dynamically and automatically sizing) string.
+    st.info(content)
