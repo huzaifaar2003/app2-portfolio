17dc9f5f95b567e9aa8aff6a4a33219852516dbd
Huzaifa <huzaifaar2003@gmail.com>
2025-04-23 06:26:55 +0500

    Added a padding column between col3 & col4.  Changed width ratio of col1 & col2.

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import webbrowser
 
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([1.5,2]) #rezised columns
 
 with col1:
     st.image("images/HAR_photo.jpg")
@@ -14,7 +14,7 @@ with col2:
     content ="""Hi! Hope you're having a great day! Huzaifa Abdul Rab here. 
     I'm a 2025 Mechatronics Engineering Graduate from NUST. 
     Gradually shifting from Mechatronics to the Fields of Data In Sha Allah, I've started by learning Python.
-    Join me on learning journey!  
+    Join me on my learning journey!  
     """
     # """...""" for multiline (dynamically and automatically sizing) string.
     st.info(content)
@@ -26,21 +26,23 @@ st.write(content2,
 df = pd.read_csv("data.csv", sep=";")
 
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5,0.75,1.5]) #Added empty column for "padding"
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(f"{index+1} - {row["title"]}")
         st.subheader(row["description"])
-        st.write(row["url"])
+        st.write(f"[Source Code]({row["url"]})")
+        #st.write(f"[Text to be displayed](link to be embedded)")
         st.image(f"images/{row["image"]}")
-        st.write("__________", font = "Helvetica")
+        st.write("___", font = "Helvetica")
+        #"___" (and any more underscores "_") makes a horizontal line across the column
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(f"{index+1} - {row["title"]}")
         st.subheader(row["description"])
-        st.write(row["url"])
+        st.write(f"[Source Code]({row["url"]})")
         st.image(f"images/{row["image"]}")
         st.write("__________", font="Helvetica")
